feaca19ffb1b2145f646b65f990e2ab13d5ca122
Hat Dao <enjoy3013@gmail.com>
2019-06-13 11:13:20 +0700

    Init

---
+++ lib/url_tracking_api_client.rb
@@ -0,0 +1,10 @@
+require "url_tracking_api_client/version"
+
+module UrlTrackingApiClient
+  class Error < StandardError; end
+  # Your code goes here...
+  #
+  def track(params: {})
+    'https://google.com'
+  end
+end
---
+++ spec/url_tracking_api_client_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe UrlTrackingApiClient do
+  it "has a version number" do
+    expect(UrlTrackingApiClient::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
